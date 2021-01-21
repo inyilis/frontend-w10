50cdd13b6567ac87d11a4a1c78d1c94399a39ef6
Inyilis Punya <inyilispunya@gmail.com>
2021-01-21 15:01:10 +0700

    up readme

--- src/views/home.vue
+++ src/views/home.vue
@@ -29,40 +29,6 @@
         </div>
         <div v-if="filter" class="col-12 text-center border-top pt-3 mt-3">
           <Sorted @sorted="sortedData" />
-          <!-- <div class="row">
-            <div class="col-6 col-md-3 d-flex justify-content-center mt-3">
-              <h5>Name</h5>
-              <select class="form-select ml-3" aria-label="Default select example" v-model="sorted.nama" @click="sortedProduct()">
-                <option selected></option>
-                <option value="ASC">A - Z</option>
-                <option value="DESC">Z - A</option>
-              </select>
-            </div>
-            <div class="col-6 col-md-3 d-flex justify-content-center mt-3">
-              <h5>Price</h5>
-              <select class="form-select ml-3" aria-label="Default select example" v-model="sorted.harga" @click="sortedProduct()">
-                <option selected></option>
-                <option value="DESC">High</option>
-                <option value="ASC">Low</option>
-              </select>
-            </div>
-            <div class="col-6 col-md-3 d-flex justify-content-center mt-3">
-              <h5>Category</h5>
-              <select class="form-select ml-3" aria-label="Default select example" v-model="sorted.kategori" @click="sortedProduct()">
-                <option selected></option>
-                <option value="ASC">Drinks</option>
-                <option value="DESC">Foods</option>
-              </select>
-            </div>
-            <div class="col-6 col-md-3 d-flex justify-content-center  mt-3">
-              <h5>Products</h5>
-              <select class="form-select ml-3" aria-label="Default select example" v-model="sorted.terbaru" @click="sortedProduct()">
-                <option selected></option>
-                <option value="DESC">New</option>
-                <option value="ASC">Old</option>
-              </select>
-            </div>
-          </div> -->
         </div>
       </header>
       <main class="row pt-4 bg-light">
